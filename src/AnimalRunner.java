ac5a5c5cf33f71be2beb23532d56e9b754505c39
BT_3W2_20 <yanyeef@nycstudents.net>
2023-03-21 08:37:12 -0400

    change

--- src/AnimalRunner.java
+++ src/AnimalRunner.java
@@ -4,6 +4,8 @@ public class AnimalRunner {
         System.out.println(animal.getAge());
         System.out.println(animal.getName());
         System.out.println(animal.getVaccinated());
+        animal.adopt();
+        animal.feed();
 
         Cat c1 = new Cat("Mary", 3, false, false);
         System.out.println(c1.getHasPlayedWith());
@@ -12,11 +14,15 @@ public class AnimalRunner {
         System.out.println(c1.getVaccinated());
         c1.play();
         System.out.println(c1.getHasPlayedWith());
+        c1.adopt();
+        c1.feed();
 
         Dog d1 = new Dog("Tim", 7, false, true);
         System.out.println(d1.getHasBeenWalked());
         System.out.println(d1.getAge());
         System.out.println(d1.getName());
         System.out.println(d1.getVaccinated());
+        d1.adopt();
+        d1.feed();
     }
 }
